d50b5e5873ae25d62d5e1726a04574936bd8d9a5
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-04-02 23:17:32 +0300

    Phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+// Create a function called superbowlWin
+function superbowlWin(record) {
+    const win = record.find(function(game) {
+        return game.result === "W";
+    });
+
+    // Check if a win was found
+    if (win) {
+        return win.year;
+    } else {
+        return undefined;
+    }
+}
